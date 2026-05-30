2eb4ac2c8badacc079d4b05dbbe065f01e66892c
João Lucas Alves Fernandes <joao.lucas.af.2006@gmail.com>
2026-05-30 11:24:44 -0300

    Função inserir agora funciona corretamente; Função mostrar_todos_os_conjuntos agora diz se não existem conjuntos criados;

--- main.c
+++ main.c
@@ -12,13 +12,30 @@ void inicializar_matriz(int matriz[M][N]) {
   }
 }
 
+void mostrar_conjunto(int* cont, int conjuntos[M][N], int linha) {
+  int i;
+
+  if (linha > *cont - 1) {
+    printf("Erro: o conjunto %d nao foi criado!", linha);
+    return;
+  }
+  printf("\nConjunto %d = ", linha);
+  if (conjuntos[linha][0]) {
+    for (i = 0; i < N && conjuntos[linha][i] != 0; i++)
+      printf("%d ", conjuntos[linha][i]);
+  } else
+    printf("vazio");
+  //printf("\n");
+}
+
 int busca_sequencial(int matriz[M][N], int linha, int valor /*, int colunas_preenchidas*/) {
                      
   int j; // coluna
 
   for (j = 0; matriz[linha][j] != 0 /*j < colunas_preenchidas*/; j++)
-    if (matriz[linha][j] == valor)
+    if (matriz[linha][j] == valor) {
       return 1; // encontrou o número repetido
+    }
   return 0; // não há números repetidos
 }
 
@@ -40,32 +57,20 @@ void inserir(int* cont, int conjuntos[M][N]) {
 
   for(i = 0; conjuntos[j][i] != 0; i++); // Coloca o índice i na primeira posição vazia.
 
-  printf("Digite os valores, um por vez:\n");
-  for (; i < N && valor != 0; i++) {
-    scanf("%d", &valor);
-    int eh_valor_repetido = busca_sequencial(conjuntos, j, valor);
-    if (valor && !eh_valor_repetido)
-      conjuntos[j][i] = valor;
-    else
-      i--;
-  }
-
-}
-
-void mostrar_conjunto(int* cont, int conjuntos[M][N], int linha) {
-  int i;
-
-  if (linha > *cont - 1) {
-    printf("Erro: o conjunto %d nao foi criado!", linha);
-    return;
-  }
-  printf("\nConjunto %d = ", linha);
-  if (conjuntos[linha][0]) {
-    for (i = 0; i < N && conjuntos[linha][i] != 0; i++)
-      printf("%d ", conjuntos[linha][i]);
+  if (i < N) {
+    printf("Digite os valores, um por vez:\n");
+    for (; i < N && valor != 0; i++) {
+      scanf("%d", &valor);
+      int eh_valor_repetido = busca_sequencial(conjuntos, j, valor);
+      if (valor && !eh_valor_repetido) {
+	conjuntos[j][i] = valor;
+      } else if (valor) {
+	printf("Valor ja inserido. Digito outro valor\n");
+	i--;
+      }
+    }
   } else
-    printf("vazio");
-  printf("\n");
+      printf("O conjunto %d esta cheio!\n", j);
 }
 
 void mostrar_todos_os_conjuntos(int* cont, int conjuntos[M][N]) {
@@ -73,6 +78,12 @@ void mostrar_todos_os_conjuntos(int* cont, int conjuntos[M][N]) {
 
   for (i = 0; i < *cont; i++)
     mostrar_conjunto(cont, conjuntos, i);
+
+  if (!*cont)
+    printf("Nenhum conjunto foi criado\n");
+  else
+    printf("\n");
+  
 }
 
 int main() {
@@ -80,11 +91,13 @@ int main() {
   int cont = 2; // cont != 0 apenas para testes
   
   conjuntos[0][0] = 1;
-  conjuntos[0][1] = 2;
+  conjuntos[1][0] = 2;
 
   mostrar_todos_os_conjuntos(&cont, conjuntos);
   
   // mostrar_conjunto(&cont, conjuntos, 0);
+  inserir(&cont, conjuntos);
+  mostrar_todos_os_conjuntos(&cont, conjuntos);
   inserir(&cont, conjuntos);
   mostrar_todos_os_conjuntos(&cont, conjuntos);
   //  mostrar_conjunto(&cont, conjuntos, 0);
