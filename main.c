3da99de9f652140cbe9813e74d2932271889aed9
João Lucas Alves Fernandes <joao.lucas.af.2006@gmail.com>
2026-06-02 09:35:28 -0300

    Funções criadas: preencher_linha (apenas para testes); conjunto_existe; indice_esta_no_intervalo; remover_conjunto;

--- main.c
+++ main.c
@@ -12,13 +12,40 @@ void inicializar_matriz(int matriz[M][N]) {
   }
 }
 
+void preencher_linha(int matriz[M][N], int linha, int valor_inicial) {
+  int i;
+  for(i = 0; i < N; i++, valor_inicial++)
+    matriz[linha][i] = valor_inicial;
+}
+
+int conjunto_existe(int* cont, int j) {
+  if (j > *cont - 1) {
+    printf("Erro: o conjunto %d nao foi criado!", j);
+    return 0;
+  }
+  return 1;
+}
+
+int indice_esta_no_intervalo(int j, int* cont) {
+  if (j < 0 || j > M - 1) {
+    printf("Erro: digite um valor entre 0 e %d", *cont - 1);
+    return 0;
+  }
+  return 1;
+}
+
 void mostrar_conjunto(int* cont, int conjuntos[M][N], int linha) {
   int i;
 
+  /*
   if (linha > *cont - 1) {
     printf("Erro: o conjunto %d nao foi criado!", linha);
     return;
   }
+  */
+  
+  if (!conjunto_existe(cont, linha)) return;
+  
   printf("\nConjunto %d = ", linha);
   if (conjuntos[linha][0]) {
     for (i = 0; i < N && conjuntos[linha][i] != 0; i++)
@@ -34,8 +61,8 @@ int busca_sequencial(int matriz[M][N], int linha, int valor /*, int colunas_pree
 
   for (j = 0; matriz[linha][j] != 0 /*j < colunas_preenchidas*/; j++)
     if (matriz[linha][j] == valor)
-      return 1; // encontrou o número repetido
-  return 0; // não há números repetidos
+      return 1;
+  return 0;
 }
 
 void inserir(int* cont, int conjuntos[M][N]) {
@@ -44,16 +71,22 @@ void inserir(int* cont, int conjuntos[M][N]) {
   printf("Digite o indice do conjunto: ");
   scanf("%d", &j);
 
+  /*
   if (j < 0 || j > M - 1) {
     printf("Erro: digite um valor entre 0 e %d", M - 1);
     return;
   }
 
+
   if (j > *cont - 1) {
     printf("Erro: o conjunto %d nao foi criado!", j);
     return;
   }
+  */
 
+  if (!indice_esta_no_intervalo(j, cont)) return;
+  if (!conjunto_existe(cont, j)) return;
+  
   for(i = 0; conjuntos[j][i] != 0; i++); // Coloca o índice i na primeira posição vazia.
 
   if (i < N) {
@@ -64,7 +97,7 @@ void inserir(int* cont, int conjuntos[M][N]) {
       if (valor && !eh_valor_repetido) {
 	conjuntos[j][i] = valor;
       } else if (valor) {
-	printf("Valor ja inserido. Digito outro valor\n");
+	printf("Valor ja inserido. Digite outro valor\n");
 	i--;
       }
     }
@@ -93,7 +126,7 @@ void buscar_valor_matriz(int *cont, int conjuntos[M][N]) {
                       // matriz muda para 1, se não continua 0
 
   for (int i = 0; i < *cont; i++) {
-    busca_sequencial(conjuntos, i, valor_procurado);
+    //busca_sequencial(conjuntos, i, valor_procurado);
     if (busca_sequencial(conjuntos, i, valor_procurado) == 1) {
       printf("O valor %d foi encontrado no conjunto %d\n", valor_procurado, i);
       encontrado = 1;
@@ -142,12 +175,30 @@ void uniao_conjuntos(int *cont, int conjuntos[M][N]) {
   }
 }
 
+void remover_conjunto(int *cont, int conjuntos[M][N]) {
+  int i, j, k, l1;
+
+  printf("Digite a linha do conjunto a ser removido: ");
+  scanf("%d", &l1);
+
+  if (!indice_esta_no_intervalo(l1, cont)) return;
+  if (!conjunto_existe(cont, l1)) return;
+
+  for(j = l1; j < M - 1 && j < *cont; j++)
+    for(k = 0; k < N; k++) {
+      conjuntos[j][k] = conjuntos[j + 1][k];
+      conjuntos[j + 1][k] = 0;
+    }
+
+  *cont = *cont - 1;
+}
+
 int main() {
   int conjuntos[M][N] = {0};
   // conta o número de conjuntos existentes
-  int cont = 2; // cont != 0 apenas para testes
-  conjuntos[0][0] = 1;
-  conjuntos[1][0] = 2;
+  int cont = M; // cont != 0 apenas para testes
+  preencher_linha(conjuntos, 0, 1);
+  preencher_linha(conjuntos, 1, 2);
 
   mostrar_todos_os_conjuntos(&cont, conjuntos);
   
@@ -158,6 +209,12 @@ int main() {
   mostrar_todos_os_conjuntos(&cont, conjuntos);
   //  mostrar_conjunto(&cont, conjuntos, 0);
   //  mostrar_conjunto(&cont, conjuntos, 1);
+
+  printf("Testando remocao de conjunto\n");
+  mostrar_todos_os_conjuntos(&cont, conjuntos);
+  remover_conjunto(&cont, conjuntos);
+  printf("Cont: %d\n", cont);
+  mostrar_todos_os_conjuntos(&cont, conjuntos);
 
   printf("Testando busca de valores ");
   buscar_valor_matriz(&cont, conjuntos);
